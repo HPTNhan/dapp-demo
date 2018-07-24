a24c8dc0fabffba80b3a894d56e6a376870d6555
nhanho <nhanho@kms-technology.com>
2018-07-24 22:44:32 +0700

    auction-dapp

---
+++ auction-dapp/src/web3.js
@@ -0,0 +1,5 @@
+﻿//import Web3 from 'C:\\\\Users\\\\NhanHo\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\web3';
+import Web3 from 'c:\\Data\\projects\\Auction\\auction-dapp\\node_modules\\web3\\';
+//import Web3 from './web3';
+const web3 = new Web3(window.web3.currentProvider);
+export default web3;
