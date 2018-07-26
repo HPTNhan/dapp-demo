0b9f8f933d52be7dce24f7e76a77667e1abfb9ba
NhanHo <hpthainhan@gmail.com>
2018-07-26 22:48:31 +0700

    config library web3

--- auction-dapp/src/web3.js
+++ auction-dapp/src/web3.js
@@ -1,5 +1,3 @@
-﻿//import Web3 from 'C:\\\\Users\\\\NhanHo\\\\AppData\\\\Roaming\\\\npm\\\\node_modules\\\\web3';
-import Web3 from 'c:\\Data\\projects\\Auction\\auction-dapp\\node_modules\\web3\\';
-//import Web3 from './web3';
+﻿import Web3 from '../node_modules/web3';
 const web3 = new Web3(window.web3.currentProvider);
 export default web3;
